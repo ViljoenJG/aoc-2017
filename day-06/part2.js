1795ac7ba34a83ba069385661d105a6b5d118e08
Kobus Viljoen <kobusv@gmail.com>
2017-12-12 15:12:13 +0200

    day 6 part2

---
+++ day-06/part2.js
@@ -0,0 +1,27 @@
+let memoryBanks = `10	3	15	10	5	15	5	15	9	2	5	8	5	2	3	6`.split('\t').map(x => Number(x));
+let configUsed = false;
+let usedConfigs = {};
+let cycles = 0;
+
+while(!configUsed) {
+  cycles++;
+  let max = Math.max(...memoryBanks);
+  let idx = memoryBanks.findIndex(x => x === max);
+  memoryBanks[idx++] = 0;
+
+  while(max) {
+    if (!(idx < memoryBanks.length)) idx = 0;
+    memoryBanks[idx++]++;
+    max--;
+  }
+
+  let arrangement = memoryBanks.join('');
+  if (usedConfigs[arrangement]) {
+    configUsed = true;
+    let recurrence = cycles - usedConfigs[arrangement];
+    console.log(recurrence)
+  }
+  usedConfigs[arrangement] = cycles;
+}
+
+console.log(cycles);
