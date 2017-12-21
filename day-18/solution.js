2fa364c18a0d02070fdc5faa9e32298ee3f4592a
Kobus Viljoen <kobusv@gmail.com>
2017-12-21 10:34:34 +0200

    Day 18, part 2

--- day-18/solution.js
+++ day-18/solution.js
@@ -1,14 +1,11 @@
 function getInstructions(input) {
-  return input
-    .split('\n')
-    .map(x =>
-      x.split(' '));
+  return input.split('\n').map(x => x.split(' '));
 }
 
 function part1(input) {
   let registers = {};
   let instructions = getInstructions(input);
-  let recovered = -1;
+  let recovered = 0;
 
   for (let a=0; a<instructions.length; ) {
     if (0 > a > instructions.length + 1) break;
@@ -17,23 +14,16 @@ function part1(input) {
         val = Number.isNaN(Number(instructions[a][2])) ?
           registers[instructions[a][2]] : Number(instructions[a][2]);
 
-    if (ins === 'set')
-      registers[reg] = val;
-    else if (ins === 'add')
-      registers[reg] += val;
-    else if (ins === 'mul')
-      registers[reg] *= val;
-    else if (ins === 'mod')
-      registers[reg] %= val;
-    else if (ins === 'rcv') {
+    if (ins === 'set') registers[reg] = val;
+    if (ins === 'add') registers[reg] += val;
+    if (ins === 'mul') registers[reg] *= val;
+    if (ins === 'mod') registers[reg] %= val;
+    if (ins === 'jgz') a += registers[reg] > 0 ? val : 1;
+    if (ins === 'rcv') {
       if (registers[reg] > 0) return recovered;
       registers[reg] = recovered;
     }
-    else if (ins === 'jgz')
-      a += registers[reg] > 0 ? val : 1;
-    else if (ins === 'snd')
-      recovered = registers[reg];
-
+    if (ins === 'snd') recovered = registers[reg];
     if (ins !== 'jgz') a++;
   }
 
@@ -41,8 +31,92 @@ function part1(input) {
 }
 
 function part2(input) {
+  let instructions = getInstructions(input);
+  let prog0 = new Program(0, instructions);
+  let prog1 = new Program(1, instructions, prog0);
 
+  while(true) {
+    prog0.run();
+    prog1.run();
+
+    if (prog0.isLocked() && prog1.isLocked()) {
+      break;
+    }
+  }
+
+  return prog1.getSentCount();
 }
+
+function Program(id, instructions, link) {
+  this._id = id;
+  this._registers = { p: id };
+  this._instructions = instructions;
+  this._queue = [];
+  this._idx = 0;
+  this._sent = 0;
+  this._locked = false;
+  this._linked = null;
+
+  if (link) {
+    this.link(link);
+  }
+}
+
+Program.prototype.isLocked = function() {
+  return this._locked;
+};
+
+Program.prototype.link = function(program) {
+  this._linked = program;
+  program._linked = this;
+};
+
+Program.prototype.send = function(val) {
+  this._linked._queue.push(val);
+  this._sent++;
+};
+
+Program.prototype.getSentCount = function() {
+  return this._sent;
+};
+
+Program.prototype.getValue = function (key) {
+  let val = Number(key);
+  return isNaN(val) ? this._registers[key] : val;
+};
+
+Program.prototype.run = function () {
+  this._locked = true;
+
+  while(0 <= this._idx < this._instructions.length) {
+    const [ins, a, b] = this._instructions[this._idx];
+
+    if (ins === 'set')
+      this._registers[a] = this.getValue(b);
+    if (ins === 'add')
+      this._registers[a] += this.getValue(b);
+    if (ins === 'mul')
+      this._registers[a] *= this.getValue(b);
+    if (ins === 'mod')
+      this._registers[a] %= this.getValue(b);
+    if (ins === 'jgz')
+      this._idx += this.getValue(a) > 0 ? this.getValue(b) : 1;
+    if (ins === 'snd')
+      this.send(this.getValue(a));
+    if (ins === 'rcv') {
+      if (this._queue.length > 0) {
+        this._registers[a] = this._queue.shift();
+      } else {
+        return;
+      }
+    }
+
+    if (ins !== 'jgz') this._idx++;
+
+    this._locked = false;
+  }
+};
+
 
 module.exports = {
   part1,
--- day-18/test.js
+++ day-18/test.js
@@ -1,6 +1,6 @@
 const { part1, part2 } = require('./solution');
 
-const mockInput = `set a 1
+const mockInput1 = `set a 1
 add a 2
 mul a a
 mod a 5
@@ -11,8 +11,20 @@ jgz a -1
 set a 1
 jgz a -2`;
 
+const mockInput2 = `snd 1
+snd 2
+snd p
+rcv a
+rcv b
+rcv c
+rcv d`
+
 describe('Day 18', () => {
   test('Part 1', () => {
-    expect(part1(mockInput)).toBe(4)
-  })
+    expect(part1(mockInput1)).toBe(4)
+  });
+
+  test('Part 2', () => {
+    expect(part2(mockInput2)).toBe(3)
+  });
 })
